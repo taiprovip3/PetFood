229bc8422666b02762a26ac463610d224bb637fa
NVPL1234 <nlam39784@gmail.com>
2022-05-14 16:43:23 +0700

    xong crud cart

--- src/main/java/springboot/petfood/entity/User.java
+++ src/main/java/springboot/petfood/entity/User.java
@@ -30,11 +30,6 @@ public class User {
 	@Column(name="last_name")
 	private String lastName;
 
-<<<<<<< HEAD
-	@OneToMany(mappedBy = "user")
-	private List<Cart> carts;
-	
-=======
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="role_id")
 	@OnDelete(action = OnDeleteAction.CASCADE)
@@ -48,7 +43,6 @@ public class User {
 		this.role = role;
 	}
 
->>>>>>> ffadcf35dcc459fae72f1303702ae83bd31f5013
 	public int getUserId() {
 		return userId;
 	}
@@ -97,39 +91,24 @@ public class User {
 		this.lastName = lastName;
 	}
 
-<<<<<<< HEAD
-	public List<Cart> getCarts() {
-		return carts;
-	}
-
-	public void setCarts(List<Cart> carts) {
-		this.carts = carts;
-	}
-	
-	public User() {
-	}
-=======
 	public User() {	}
->>>>>>> ffadcf35dcc459fae72f1303702ae83bd31f5013
 
 	public User(int userId) {
 		this.userId = userId;
 	}
 
-	public User(int userId, String username, String password, String email, String firstName, String lastName,
-			List<Cart> carts) {
+	public User(int userId, String username, String password, String email, String firstName, String lastName) {
 		this.userId = userId;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.carts = carts;
 	}
 
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", email=" + email
-				+ ", firstName=" + firstName + ", lastName=" + lastName + ", carts=" + carts + "]";
+				+ ", firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
 }
